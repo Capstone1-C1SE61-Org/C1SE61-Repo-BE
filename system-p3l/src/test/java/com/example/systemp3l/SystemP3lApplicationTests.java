cc11be28f89f4e939bc13a399e3d0f3970814fa9
tienthanh11 <dotienthanh1121@gmail.com>
2024-10-15 11:37:07 +0700

    Initial commit

---
+++ system-p3l/src/test/java/com/example/systemp3l/SystemP3lApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.systemp3l;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SystemP3lApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
