64af5edc0e973443b3a1089feac0d926a8201a9f
tienthanh11 <dotienthanh1121@gmail.com>
2024-10-17 15:56:53 +0700

    Initial commit

---
+++ system-p3l/src/main/java/com/example/systemp3l/SystemP3lApplication.java
@@ -0,0 +1,13 @@
+package com.example.systemp3l;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SystemP3lApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SystemP3lApplication.class, args);
+    }
+
+}
