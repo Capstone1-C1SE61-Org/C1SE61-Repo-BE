93dfbfe1cd9bf8e335c763c66a2d8c0e05833983
tienthanh11 <dotienthanh1121@gmail.com>
2024-11-07 11:52:08 +0700

    dev add 7/11/2024

--- system-p3l/src/main/java/com/example/systemp3l/repository/IAccountRepository.java
+++ system-p3l/src/main/java/com/example/systemp3l/repository/IAccountRepository.java
@@ -2,10 +2,19 @@ package com.example.systemp3l.repository;
 
 import com.example.systemp3l.model.Account;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
 
 @Repository
 @Transactional
 public interface IAccountRepository extends JpaRepository<Account, Integer> {
+
+    @Query(value = "select account_id, username, encrypt_password, email, is_enable " +
+            "from account " +
+            "where username = :username and is_enable = true", nativeQuery = true)
+    Optional<Account> findAccountByUsername(@Param("username") String username);
 }
